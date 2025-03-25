f36c6e2688bb2c3f2e2b72c09798a29851537f37
cjdango <31501132+cjdango@users.noreply.github.com>
2025-03-25 21:11:43 +0800

    install zen-browser

--- install.sh
+++ install.sh
@@ -5,8 +5,12 @@ brew install --cask ghostty
 brew install --cask nikitabobko/tap/aerospace
 
 # https://github.com/FelixKratz/JankyBorders?tab=readme-ov-file#install
-brew tap FelixKratz/formulae
-brew install borders
+brew tap FelixKratz/formulae && brew install borders
+
+# https://github.com/zen-browser/desktop?tab=readme-ov-file#macos
+brew install --cask zen-browser
 
 # https://jetify-com.vercel.app/docs/devbox/installing_devbox/?install-method=macos
 curl -fsSL https://get.jetify.com/devbox | bash
+
+echo "## Installation done!" | gum format
